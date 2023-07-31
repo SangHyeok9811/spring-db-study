ecc43d259d80214d70431726f030eb75adaf1c0b
Sanghyeok <Sanghyeok Han>
2023-07-31 11:23:52 +0900

    1

--- myapp/src/main/java/com/hsh/myapp/contact/Contact.java
+++ myapp/src/main/java/com/hsh/myapp/contact/Contact.java
@@ -1,5 +1,7 @@
 package com.hsh.myapp.contact;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +11,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor // 전체 필드 초기 생성자
 @NoArgsConstructor // 빈 생성자
+@Entity
 public class Contact {
+
+    @Id
+    private String email;   // key
+
     private String name;
     private String phone;
-    private String email;   // key
     // 파일을 base64 data-url 문자열로 저장
     private String image;
 }
